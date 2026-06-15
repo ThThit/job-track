db781af36e97ef8f4a164bb5418228e8da4ab23a
thuhtinthit <thuhtinthit@gmail.com>
2026-06-16 03:46:50 +0700

    mongodb connection

---
+++ lib/db.ts
@@ -0,0 +1,53 @@
+import mongoose, { mongo, Mongoose } from "mongoose";
+import { cache } from "react";
+
+const MONGODB_URI = String(process.env.MONGODB_URI);
+
+if (!MONGODB_URI) {
+    throw new Error(
+        "Please define the MONGODB_URI environment variable inside .evn"
+    );
+}
+
+interface MongooseCache {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+    var mongoose: MongooseCache | undefined;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+    global.mongoose = cached;
+}
+
+// connection
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+}
+
+export default connectDB;
